54c7a1dfa219f432e39e46df5154c9760421054c
JorgeGM <jgmmolla@hotmail.com>
2024-05-07 14:51:51 +0200

    Añadida Base de Datos Persistente

--- src/main/java/com/example/demo/classes/HotelRepository.java
+++ src/main/java/com/example/demo/classes/HotelRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.classes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
